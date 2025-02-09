7cd60ffef29f664e8dd22c322eb44efdbf6e3f94
Alexander Gorobets <for.gorobets@yandex.ru>
2025-02-09 23:18:23 +0300

    Устраняем ошибку запуска сервера на гите

--- stat/stat-service/src/main/java/ru/practicum/StatsServiceApplication.java
+++ stat/stat-service/src/main/java/ru/practicum/StatsServiceApplication.java
@@ -3,11 +3,9 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "ru.practicum")
+@SpringBootApplication
 public class StatsServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StatsServiceApplication.class, args);
     }
-
 }
