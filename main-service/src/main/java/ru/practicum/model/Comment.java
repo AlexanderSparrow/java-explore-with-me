2d4b3b491d9835eac254bc3e666c1dd84873f201
Alexander Gorobets <for.gorobets@yandex.ru>
2025-04-12 11:58:57 +0300

    Разработка фичи "Комментарии к событиям". Создана сущность, скрипт таблицы

---
+++ main-service/src/main/java/ru/practicum/model/Comment.java
@@ -0,0 +1,35 @@
+package ru.practicum.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(nullable = false)
+    private LocalDateTime created;
+
+}
